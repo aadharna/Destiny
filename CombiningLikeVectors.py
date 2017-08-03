a4e781eb2e513177eeaf383e101cf1eb13d5555a
Aaron Dharna <adharna@fordham.edu>
2017-08-03 17:22:28 -0500

    [update file] added comments

--- CombiningLikeVectors.py
+++ CombiningLikeVectors.py
@@ -1,41 +1,31 @@
 import pandas as pd
 
-def normalize(df):
-    result = df.copy()
-    for feature_name in df.columns:
-        max_value = df[feature_name].max()
-        min_value = df[feature_name].min()
-        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
-    return result
-
+#PARTS OF FILE NAMES
 FileNameP1 = "indexByMatch_batch"
 FileNameP2 = "Udacity_103"
 FileNameP3 = "Udacity_205"
 SUFFIX = ".csv"
+FileNameP4 = "batch"
+FileNameP5 = "Udacity"
 
-df11 = pd.read_csv(FileNameP1 + FileNameP2 + "_TOTAL" + SUFFIX)
-df12 = pd.read_csv(FileNameP1 + FileNameP3 + "_TOTAL" + SUFFIX)
-df11.drop("Unnamed: 0", axis=1, inplace=True)
-df12.drop("Unnamed: 0", axis=1, inplace=True)
+df1 = pd.read_csv(FileNameP1 + "1" + FileNameP2 + SUFFIX, index_col="Unnamed: 0")
+#df2 = pd.read_csv(FileNameP1 + FileNameP3 + "_TOTAL" + SUFFIX, index_col="Unnamed: 0")
 
-#
-# for i in range(14, 15):
-#
-#     df103 = pd.read_csv(FileNameP1 + str(i) + FileNameP2 + SUFFIX)
-#     df205 = pd.read_csv(FileNameP1 + str(i) + FileNameP3 + SUFFIX)
-#
-#     df103.drop("Unnamed: 0", axis=1, inplace=True)
-#     df205.drop("Unnamed: 0", axis=1, inplace=True)
-#
-#     df11 = df11.append(df103)
-#     df12 = df12.append(df205)
+#For all of the files that I have separated.
+for i in range(2, 15):
+    #Read in the next file and save it to the first one.
+
+    df103 = pd.read_csv(FileNameP1 + str(i) + FileNameP2 + SUFFIX, index_col="Unnamed: 0")
+    #df205 = pd.read_csv(FileNameP1 + str(i) + FileNameP3 + SUFFIX, index_col="Unnamed: 0")
 
 
-df11 = normalize(df11)
-df12 = normalize(df12)
+    df1 = df1.append(df103)
+    #df2 = df2.append(df205)
 
 
-df11.to_csv(FileNameP1 + FileNameP2 + "_TOTAL_NORM" + SUFFIX)
-df12.to_csv(FileNameP1 + FileNameP3 + "_TOTAL_NORM" + SUFFIX)
+#Save the one large Dataframe to a CSV
+df1.to_csv(FileNameP4 + FileNameP5 + "_TOTAL" + SUFFIX)
+#df2.to_csv(FileNameP1 + FileNameP3 + "_TOTAL" + SUFFIX)
 
-print(df11.shape, df12.shape)
+#Print the number of matches now saved in the one file.
+print(df1.shape)
